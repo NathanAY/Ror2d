fc8c3b8c123d3ca14b0d49d7f1f510ac3d0bd64d
Vladislav Yanchevsky <v.yanchevsky@itransition.com>
2020-12-19 21:08:52 +0300

    Add range to prevent creating endless bullet objects

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -6,10 +6,13 @@ public class BulletMovement : MonoBehaviour
 {
 
     public float speed = 20;
+    private float range;
     private Vector3 shootDir;
+    private float proccedRange = 0;
 
-    public void SetupDirection(Vector3 startingPoint, Vector3 targetPoint)
+    public void SetupDirection(Vector3 startingPoint, Vector3 targetPoint, float range)
     {
+        this.range = range;
         transform.position = startingPoint;
         shootDir = (targetPoint - startingPoint).normalized;
         transform.eulerAngles = new Vector3(0, 0, CalculateEulerAngles(shootDir));
@@ -18,6 +21,8 @@ public class BulletMovement : MonoBehaviour
     void Update()
     {
         transform.position += shootDir * speed * Time.deltaTime;
+        proccedRange += speed * Time.deltaTime;
+        CheckToDestroy();
     }
 
     private float CalculateEulerAngles(Vector3 dir)
@@ -26,5 +31,13 @@ public class BulletMovement : MonoBehaviour
         float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         if (n < 0) n += 360;
         return n;
+    }
+
+    private void CheckToDestroy()
+    {
+        if (proccedRange > range)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,12 +9,13 @@ public class Weapon : ScriptableObject
     public GameObject bulletPrefab;
     public float fireRate = 1;
     public int damage = 20;
-    
+    public float range = 100;
+
     public void Shoot()
     {
         GameObject firePoint = GameObject.Find("FirePoint");
         GameObject direction = GameObject.Find("Dir");
         GameObject bullet = Instantiate(bulletPrefab, firePoint.transform.position, Quaternion.identity);
-        bullet.GetComponent<BulletMovement>().SetupDirection(firePoint.transform.position, direction.transform.position);
+        bullet.GetComponent<BulletMovement>().SetupDirection(firePoint.transform.position, direction.transform.position, range);
     }
 }
