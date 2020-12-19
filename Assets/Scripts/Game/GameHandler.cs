c9cb6688cce900d98e8757f44b24a9fd3e15a4fa
Artyom Yanchevsky <artyom.yanchevsky@duallab.com>
2020-12-19 23:56:29 +0300

    Set max fps to 120 at Unity Editor

--- Assets/Scripts/Game/GameHandler.cs
+++ Assets/Scripts/Game/GameHandler.cs
@@ -15,6 +15,10 @@ public class GameHandler : MonoBehaviour
         CameraFollow.Setup(() => plyerTransform.position);
         Debug.Log("GameHandler.Start");
 
+        #if UNITY_EDITOR
+                QualitySettings.vSyncCount = 0;  // VSync must be disabled
+                Application.targetFrameRate = 60; // targetFrameRate 60 = 120 fps
+        #endif
         // int i = 0;
         // FunctionPeriodic.Create(() => {
             // CMDebug.TextPopupMouse("Ding "+i+"!");
